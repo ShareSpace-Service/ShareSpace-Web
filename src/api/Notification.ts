b2d24fcf5600e399e3eb8b28cca4f0e2ab415430
이용학 <leeyonghak13@gmail.com>
2024-10-23 15:34:26 +0900

    #3 [CHORE] - Notification.ts에 Request.ts 적용

--- src/api/Notification.ts
+++ src/api/Notification.ts
@@ -1,18 +1,12 @@
+import { getRequest } from './Request'; // getRequest 함수 사용
+
 /**
  * 서버에서 알림 데이터를 GET 요청으로 불러오는 함수
  *
- * @returns {Promise<Notification[]>} 알림 리스트 데이터를 반환하는 Promise
- * @throws {Error} 서버 응답이 성공적이지 않을 경우 에러
+ * @returns {Promise<any>} 서버로부터 알림 리스트 데이터를 반환하는 Promise
+ * @throws {Error} 서버 응답이 성공적이지 않을 경우 에러 발생
  */
-export async function fetchNotifications() {
-  const response = await fetch('http://localhost:8080/notification', {
-    method: 'GET',
-  });
-
-  if (!response.ok) {
-    throw new Error('서버 상태가 그냥 미누갓!');
-  }
-
-  const result = await response.json();
-  return result.data; // 알림 데이터는 응답의 data 속성에 있으므로
+export async function fetchNotifications(): Promise<any> {
+  const result = await getRequest('http://localhost:8080/notification');
+  return result.data;
 }
