2e36b10fdf1a476157f05173dbfe67b529c223e4
이용학 <leeyonghak13@gmail.com>
2024-10-26 02:18:50 +0900

    #6 [CHORE] - 로그인 및 로그아웃시 모달 띄워지도록 변경
    - 로그인 개선 : Enter키 눌러도 로그인 되도록 변경
    - 로그인 실패시 응답의 Message를 받는 모달 창을 띄워주도록 변경
    - 로그아웃 클릭시 한 번 더 확인하는 모달창이 띄워지며, 로그아웃이 성공하면 login 페이지로 리디렉션 하게끔 구현

--- src/component/ui/CustomModal.tsx
+++ src/component/ui/CustomModal.tsx
@@ -1,25 +1,54 @@
+import { useEffect } from 'react';
 import ButtonProps from '@/component/ui/ButtonProps';
-import { Link } from 'react-router-dom';
 
 interface ModalProps {
   title: string;
   description?: string;
   confirmText: string;
-  cancelText: string;
-  confirmLink: string;
-  cancelLink: string;
+  cancelText?: string;
+  onConfirm?: () => void;
+  onCancel?: () => void;
 }
 
+/**
+ * CustomModal 컴포넌트는 확인 및 취소 버튼을 포함한 모달을 표시하며,
+ * ESC 키나 모달 외부 클릭 시 모달을 닫을 수 있음.
+ */
 function CustomModal({
   title,
   description,
   confirmText,
   cancelText,
-  confirmLink,
-  cancelLink,
+  onConfirm,
+  onCancel,
 }: ModalProps) {
+  // ESC 키로 모달 닫기
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === 'Escape') {
+        onCancel && onCancel();
+      }
+    };
+    window.addEventListener('keydown', handleKeyDown);
+
+    // Cleanup function: 컴포넌트가 사라질 때 이벤트 리스너를 제거
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [onCancel]);
+
+  // 모달 외부 클릭 시 모달 닫기
+  const handleBackdropClick = (e: React.MouseEvent) => {
+    if (e.target === e.currentTarget) {
+      onCancel && onCancel();
+    }
+  };
+
   return (
-    <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-50">
+    <div
+      className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-50"
+      onClick={handleBackdropClick} // 외부 클릭 시 닫기
+    >
       <div className="signUpBg w-[500px] h-[300px] p-6 rounded-lg relative">
         <div className="flex flex-col h-full justify-around">
           <div>
@@ -31,12 +60,14 @@ function CustomModal({
             )}
           </div>
           <div className="flex items-center gap-3 justify-around">
-            <Link to={confirmLink}>
+            <button onClick={onConfirm}>
               <ButtonProps title={confirmText} size="check" variant="custom" />
-            </Link>
-            <Link to={cancelLink}>
-              <ButtonProps title={cancelText} size="check" variant="custom" />
-            </Link>
+            </button>
+            {cancelText && (
+              <button onClick={onCancel}>
+                <ButtonProps title={cancelText} size="check" variant="custom" />
+              </button>
+            )}{' '}
           </div>
         </div>
       </div>
