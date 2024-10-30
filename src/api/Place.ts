61b14ec03ab53af41738de804ca6f29e494f9cc8
hyeone22 <seunghyeone2@gmail.com>
2024-10-30 15:48:45 +0900

    #2 [FEAT] - 장소 목록 조회 API 수정

--- src/api/Place.ts
+++ src/api/Place.ts
@@ -11,6 +11,21 @@ import { getRequest } from './Request';
  *
  * @returns {Promise<Place[]>} 서버로부터 Place 리스트 데이터를 반환하는 Promise
  */
+export async function fetchProductList() {
+  const result: ApiResponse = await getRequest('http://localhost:8080/place');
+  if (result.success && result.data) {
+    console.log('요청 성공', result.message);
+    return result.data;
+  } else {
+    console.error('API 요청 실패', result);
+  }
+}
+
+/**
+ * 서버에서 Place 데이터를 GET 요청으로 불러오는 함수
+ *
+ * @returns {Promise<Place[]>} 서버로부터 Place 리스트 데이터를 반환하는 Promise
+ */
 export async function fetchPlaceList(productId: number): Promise<Place[]> {
   try {
     const result: ApiResponse = await getRequest(
